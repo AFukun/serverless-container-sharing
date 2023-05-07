e578ba7687303ebcd4d6dc97fe05f69632ec8adb
AFukun <AFukun@AFsMacBook>
2023-05-08 01:22:31 +0800

    success server deployment using imgclsmob

--- setup/setup.py
+++ setup/setup.py
@@ -10,4 +10,6 @@ parser.add_argument("-D", "--data-dir")
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    utils.gen_model_data(args.data_dir)
+    model_list = ["resnet50", "vgg16", "vgg19"]
+
+    utils.gen_model_data(args.data_dir, model_list)
--- tests/01_generate_local_data_test.py
+++ tests/01_generate_local_data_test.py
@@ -8,7 +8,7 @@ import utils
 os.chdir("setup")
 
 data_dir = "../tmp/"
-shutil.rmtree(data_dir)
-os.mkdir(data_dir)
+# shutil.rmtree(data_dir)
+# os.mkdir(data_dir)
 model_name_list = ["resnet50", "vgg16", "vgg19"]
 utils.gen_model_data(data_dir, model_name_list)
