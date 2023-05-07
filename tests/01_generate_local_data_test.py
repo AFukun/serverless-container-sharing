10db82ed9c56c541bf1c9d1aaa4262c561092080
AFukun <AFukun@AFsMacBook>
2023-05-07 21:43:00 +0800

    refine 01_test

--- tests/01_generate_local_data_test.py
+++ tests/01_generate_local_data_test.py
@@ -1,10 +1,14 @@
 import os
 import sys
+import shutil
 
 sys.path.insert(1, "setup")
 import utils
 
 os.chdir("setup")
+
 data_dir = "../tmp/"
+shutil.rmtree(data_dir)
+os.mkdir(data_dir)
 model_name_list = ["resnet50", "vgg16", "vgg19"]
 utils.gen_model_data(data_dir, model_name_list)
